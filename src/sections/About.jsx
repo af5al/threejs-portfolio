1862d479b9248c99999570ed088b684b2c74ff1b
Afi-THEdeveloper <mohdafsal1049@gmail.com>
2025-03-20 09:04:50 +0530

    first commit

---
+++ src/sections/About.jsx
@@ -0,0 +1,115 @@
+import { useState } from 'react';
+import Globe from 'react-globe.gl';
+
+import Button from '../components/Button.jsx';
+
+const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText('afiafsal6023@gmail.com');
+    setHasCopied(true);
+
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <section className="c-space my-20" id="about">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">Hi, I’m Mohamed Afsal</p>
+              <p className="grid-subtext">
+                Highly motivated and resourceful Full Stack Developer with 1+ year of working experience delivering
+                scalable and feature-rich applications using the MERN stack and PERN stack.Possesses a strong track
+                record of successful projects, including an ERP system for bussiness solutions like SAP, an E-commerce
+                site and an Event managment social media web app. Energetic, adaptable, and a quick learner with a
+                resilient mindset, dedicated to crafting API-friendly solutions.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">
+                Proficient in frontend languages like HTML5, CSS3, JavaScript, React.js, Redux, mobx, and backend tools
+                like Express.js, Node.js, with databases MongoDB and Postgres SQL.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
+              <p className="grid-subtext">I&apos;m based in Kerala, India and open to work worldwide.</p>
+              <a href="#contact" className="w-fit">
+                <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">
+                I love solving problems and building things through code. Programming isn&apos;t just my
+                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                  afiafsal6023@gmail.com
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
